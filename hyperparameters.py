bed92b47bf408dc0ab5d089e86b7370c1c4b1a26
imraunav <raun.ghosh@gmail.com>
2023-11-03 12:49:53 +0530

    Trial run of 100 epochs

--- hyperparameters.py
+++ hyperparameters.py
@@ -1,12 +1,18 @@
 dataset_path = "./CTP_Wires_Chargers_etc"
 batch_size = 24
 num_workers = 8 # number of cores/processes on CPU to load data
-epochs = 10
+epochs = 100
 crop_size = 64
 
 learning_rate_init = 2e-3
 lam = 0.5
 eta = 1.2
 decay_rate = 0.9
+
+I_max = 20 # max steps to train the network
+L_max = 1.8
+L_min = 1.2
+
+checkpoint_epoch = 10
 
 sample_trial = 5
--- train.py
+++ train.py
@@ -1,85 +1,136 @@
 import torch
-from torch import nn 
+from torch import nn
 from torch.optim import Adam, lr_scheduler
 from torch.utils.data import DataLoader
+import matplotlib.pyplot as plt
 
 from model import Generator, Discriminator
-from utils import XRayDataset
+from utils import XRayDataset, TVLoss
 import hyperparameters
 
 
-EPS = 1e-8 
+bce = nn.BCELoss()
+mse = nn.MSELoss()
+l1 = nn.L1Loss()
+tv = TVLoss
 
-def gen_loss(D1_fake, D2_fake):
-    # Loss for Generator
-    G_loss_GAN_D1 = -torch.mean(torch.log(D1_fake + EPS))
-    G_loss_GAN_D2 = -torch.mean(torch.log(D2_fake + EPS))
-    G_loss_GAN = G_loss_GAN_D1 + G_loss_GAN_D2
-    return G_loss_GAN
+
+def content_loss(real_im, gen_im):
+    # may need to play around with this
+    # return mse(real_im, gen_im)
+    eta = hyperparameters.eta
+    return l1(real_im, gen_im) + eta * tv(real_im - gen_im)
+
+
+def generator_loss(score):
+    loss = bce(torch.ones_like(score), score)
+    return loss
+
+
+def discriminator_loss(score_real, score_gen):
+    real_loss = bce(torch.ones_like(score_real), score_real)
+    gen_loss = bce(torch.zeros_like(score_gen), score_gen)
+
+    return real_loss + gen_loss
+
 
 def train(generator, disc_l, disc_h, loader, epochs, device):
     gen_epoch_loss = []
+    generator.to(device)
+    disc_l.to(device)
+    disc_h.to(device)
+
     lr = hyperparameters.learning_rate
     decay_rate = hyperparameters.decay_rate
     gen_opt = lr_scheduler.ExponentialLR(Adam(generator.parameters(), lr), decay_rate)
     disc_l_opt = lr_scheduler.ExponentialLR(Adam(disc_l.parameters(), lr), decay_rate)
     disc_h_opt = lr_scheduler.ExponentialLR(Adam(disc_h.parameters(), lr), decay_rate)
-
-    adv_crit = nn.BCEloss()
-
+    L_gmax = 1000  # just a precaution
+    epoch_loss = {"gen": [], "disc_l": [], "disc_h": []}
     for epoch in range(epochs):
         # set to training mode
         disc_l.train()
         disc_h.train()
         generator.train()
 
-        print(f"Training epoch: {epoch}")
         for bno, batch in enumerate(loader):
             low_imgs, high_imgs = batch
             # send to GPU
             low_imgs = low_imgs.to(device)
             high_imgs = high_imgs.to(device)
 
+            gen_imgs = generator(high_imgs, low_imgs)
+
             # Train Discriminators
-            disc_l_opt.zero_grad()
-            disc_h_opt.zero_grad()
+            # discriminator low energy
+            for _ in range(hyperparameters.I_max):
+                disc_l_opt.zero_grad()
+                real_lables = disc_l(low_imgs)
+                gen_lables = disc_l(gen_imgs)
+                dloss = discriminator_loss(real_lables, gen_lables)
+                dloss.backwards()
+                disc_l_opt.step()
+                if dloss.detach() <= hyperparameters.L_max:
+                    break
+            epoch_loss["disc_l"].append(dloss)
 
-            gen_imgs = generator(low_imgs, high_imgs)
-            dl_out_fake = disc_l(gen_imgs.detech()) # avoid backpropagating through generator
-            dh_out_fake = disc_h(gen_imgs.detech())
-
-            dl_out_real = disc_l(low_imgs.detech())
-            dh_out_real = disc_h(high_imgs.detech())
-
-
-            # make labels
-            fake_labels = torch.zeros(gen_imgs.shape[0], device=device)
-            real_labels = torch.ones(gen_imgs.shape[0], device=device)
-            # get loss 
-            fake_loss_l = adv_crit(dl_out_fake, fake_labels)
-            real_loss_l = adv_crit(dl_out_real, real_labels)
-            # update discriminators
-            dl_loss = fake_loss_l + real_loss_l
-            dl_loss.backward()
-            disc_l_opt.step()
-
-            fake_loss_h = adv_crit(dh_out_fake, fake_labels)
-            real_loss_h = adv_crit(dh_out_real, real_labels)
-
-            dh_loss = fake_loss_h + real_loss_h
-            dh_loss.backward()
-            disc_h_opt.step()
-
+            # discriminator high energy
+            for _ in range(hyperparameters.I_max):
+                disc_h_opt.zero_grad()
+                real_lables = disc_h(high_imgs)
+                gen_lables = disc_h(gen_imgs)
+                dloss = discriminator_loss(real_lables, gen_lables)
+                dloss.backwards()
+                disc_h_opt.step()
+                if dloss.detach() <= hyperparameters.L_max:
+                    break
+            epoch_loss["disc_h"].append(dloss)
 
             # Train Generator
-            gen_opt.zero_grad()
-            fake_labels = torch.ones(gen_imgs.shape[0], device=device)
-            dl_out_fake = disc_l(gen_imgs)
-            dh_out_fake = disc_h(gen_imgs)
-            
+            def generator_trianer():
+                gen_imgs = generator(high_imgs, low_imgs)
+                gen_opt.zero_grad()
+                cont_loss_l = content_loss(gen_im=gen_imgs, real_im=low_imgs)
+                cont_loss_h = content_loss(gen_im=gen_imgs, real_im=high_imgs)
+                score_l = disc_l(gen_imgs)
+                score_h = disc_h(gen_imgs)
+                gen_loss_l = generator_loss(score_l)
+                gen_loss_h = generator_loss(score_h)
+                gloss = (gen_loss_l + gen_loss_h) + hyperparameters.lam * (
+                    cont_loss_l + cont_loss_h
+                )
+                gloss.backwards()
+                gen_opt.step()
+                return score_l.detach(), score_h.detach(), gloss.detach()
 
+            for _ in range(hyperparameters.I_max):
+                score_l, score_h, _ = generator_trianer()
+                if score_l >= hyperparameters.L_min or score_h >= hyperparameters.L_min:
+                    break
+            for _ in range(hyperparameters.I_max):
+                _, _, gloss = generator_trianer()
+                if epoch == 0 and bno == 0:  # first loss count
+                    L_gmax = 0.8 * gloss
+                if gloss < L_gmax:
+                    break
+            epoch_loss["gen"].append(gloss)
 
+        # checkpoint
+        if (epoch + 1) % hyperparameters.checkpoint_epoch == 0:
+            torch.save(generator.state_dict(), f"./weights/generator_epoch{epoch+1}.pt")
+            torch.save(disc_l.state_dict(), f"./weights/disc_l_epoch{epoch+1}.pt")
+            torch.save(disc_h.state_dict(), f"./weights/disc_h_epoch{epoch+1}.pt")
 
+            torch.save(gen_opt.state_dict(), f"./weights/gen_opt_epoch{epoch+1}.pt")
+            torch.save(disc_l_opt.state_dict(), f"./weights/disc_l_opt_epoch{epoch+1}.pt")
+            torch.save(disc_h_opt.state_dict(), f"./weights/disc_h_opt_epoch{epoch+1}.pt")
+            print(f"Epoch: {epoch+1}")
+
+            plt.plot(epoch_loss["gen"], label="Generator loss")
+            plt.plot(epoch_loss["disc_l"], label="Disc_l loss")
+            plt.plot(epoch_loss["disc_h"], label="Disc_h loss")
+            plt.legend()
+            plt.savefig("./checkpoints/loss_plot.png")
 
             # print(low.shape, high.shape)
 
